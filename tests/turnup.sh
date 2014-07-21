8d4c081eef4c65759fa8358537062573d61e8426
Christian Svensson <blue@cmd.nu>
2014-07-21 10:37:35 +0200

    Changes to turnup and cloud testing

--- tests/turnup.sh
+++ tests/turnup.sh
@@ -2,18 +2,18 @@
 set -e
 
 GIT_BASE=${1?}
-KERNEL="3.12.6"
 
 echo "Running OpenRISC regression turnup on $HOST with repos @ $GIT_BASE"
 
 sudo apt-get update
 sudo apt-get install --yes screen git wget build-essential texinfo flex bison \
-  libmpc-dev gawk bc expect nfs-kernel-server isc-dhcp-server bridge-utils \
-  autogen
+  libmpc-dev gawk bc expect isc-dhcp-server bridge-utils \
+  autogen dejagnu
+sudo apt-get build-dep qemu
 
 sudo service isc-dhcp-server stop || true
 
-echo 'INTERFACES="or1k-br"' | sudo tee /etc/default/isc-dhcp-server
+echo 'INTERFACES="buildbot-br"' | sudo tee /etc/default/isc-dhcp-server
 sudo tee /etc/dhcp/dhcpd.conf << _EOF_
 default-lease-time 600;
 max-lease-time 7200;
@@ -25,9 +25,9 @@ subnet 172.16.0.0 netmask 255.255.255.0 {
 }
 _EOF_
 
-sudo /sbin/brctl addbr or1k-br
-sudo ip addr add 172.16.0.1/24 dev or1k-br
-sudo ip link set up dev or1k-br
+sudo /sbin/brctl addbr buildbot-br
+sudo ip addr add 172.16.0.1/24 dev buildbot-br
+sudo ip link set up dev buildbot-br
 
 sudo service isc-dhcp-server start
 
@@ -37,27 +37,20 @@ mkdir -p /srv/compilers
 mkdir -p /srv/build
 sudo mount none -t tmpfs /srv/build
 
-ssh-keygen -N '' -f ~/.ssh/or1ksim
+ssh-keygen -N '' -f ~/.ssh/id_rsa
 
 echo "Cloning .."
 
 cd /srv
 git clone $GIT_BASE/or1k-devel.git
 
-sudo tee /etc/exports << _EOF_
-/srv/or1k-devel/initramfs 172.16.0.0/16(ro,async,no_root_squash,no_subtree_check)
-/srv/or1k-devel           172.16.0.0/16(ro,async,no_subtree_check)
-/srv/build                172.16.0.0/16(ro,async,no_subtree_check,fsid=1)
-_EOF_
-sudo service nfs-kernel-server restart
-
 cd or1k-devel
 
-echo $GIT_BASE/or1k-src.git \
+echo $GIT_BASE/binutils-gdb.git \
      $GIT_BASE/or1k-gcc.git \
      $GIT_BASE/or1k-glibc.git \
      $GIT_BASE/or1k-linux.git \
-     $GIT_BASE/or1ksim.git \
+     $GIT_BASE/or1k-qemu.git \
      https://github.com/openrisc/or1k-dejagnu \
      | xargs -n 1 -P 0 git clone
 
@@ -66,21 +59,33 @@ ln -sf or1k-linux linux
 echo "Building .."
 export PATH="$PATH:/srv/compilers/openrisc-devel/bin"
 
+(cd linux; git checkout smp)
+
 make linux
+make qemu
+
+qemu-img create -f raw /srv/build/root.img 1G
+/sbin/mkfs.ext4 -F /srv/build/root.img
+
+sudo mount -o loop /srv/build/root.img initramfs/
+sudo chown bluecmd.bluecmd initramfs/
+git checkout initramfs/
+
 make root
-make or1ksim
-make dejagnu
+sudo cp tests/or1k-linux-sim.exp /usr/share/dejagnu/baseboards
+
+echo "Starting simulator .."
+sudo screen -dmS qemu-sim tests/qemu-system /srv/build/root.img
+
+echo "Waiting for simulator .."
+sleep 10
 
 cd tests
-make simulators
-echo "Waiting 30 s for the simulator instances to come up .."
-sleep 30
-
-echo "Creating SSH multiplexers .."
-make ssh
+sudo arp -n | grep buildbot-br | grep -v incomplete \
+  | cut -f 1 -d ' ' > instances
 
 echo "Testing .."
-make test
-
-echo "Testing done, halting machine"
-halt
+make test-gcc
+#
+#echo "Testing done, halting machine"
+#halt
