c50f38ee927f07ca0e3ed8b631a31788455d4760
Christian Svensson <blue@cmd.nu>
2014-07-21 20:37:09 +0200

    Turnup tweaks, qemu-system 1GiB reserved ram

--- tests/gcloud.sh
+++ tests/gcloud.sh
@@ -20,7 +20,7 @@ gcutil addinstance \
   --machine_type="$MACHINE" \
   --network=default \
   --external_ip_address=ephemeral \
-  --image="https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-7-wheezy-v20140318" \
+  --image="https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-7-wheezy-v20140619" \
   --persistent_boot_disk=true \
   --noautomatic_restart \
   --on_host_maintenance=migrate \
@@ -31,6 +31,5 @@ sleep 30
 
 gcutil push "$NAME" turnup.sh /tmp
 gcutil ssh "$NAME" sudo apt-get install --yes screen
-gcutil ssh "$NAME" screen -dmS test-turnup /tmp/turnup.sh "$GIT_BASE"
-
-
+gcutil ssh "$NAME" screen -dmS test-turnup \
+  sh -c "\"/tmp/turnup.sh \\\"$GIT_BASE\\\"; exec bash\""
